655da10b7cb931cfe63c03fb7f0e636d9bedbe2e
ww456zz <109567046+ww456zz@users.noreply.github.com>
2022-08-23 19:11:19 -0500

    Add files via upload

---
+++ app/crew/crew.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
+
+  crew: object[] = [
+    {name: "Eileen Collins", firstMission: false},
+    {name: "Mae Jemison", firstMission: false},
+    {name: "Ellen Ochoa", firstMission: true}
+  ];
+  memberBeingEdited: object = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+add(memberName:string, isFirst: boolean) {
+  this.crew.push({name:memberName, firstMission: isFirst});
+}
+remove(member : object) {
+  let idx = this.crew.indexOf(member);
+  this.crew.splice(idx,1)
+}
+
+edit(people: object) {
+  this.memberBeingEdited = people;
+}
+save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+}
